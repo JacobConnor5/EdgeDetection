c0bbddddce67e26e9b7d4f87b448c4a6c2c4d821
JacobConnor <gn24034@bristol.ac.uk>
2025-12-30 23:47:57 +0000

    working with different sizes

--- src/Main.java
+++ src/Main.java
@@ -11,42 +11,14 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
 
         Convolution conv = new Convolution();
+
+        conv.setvMatrix(20);
         ImageMatrix image = new ImageMatrix(new File("./skyscraper.jpg"));
         conv.setIMatrix(image.pixels);
         conv.Vertical();
         image.setPixels(conv.newMatrix);
 
         image.backToImage(Integer.toString(conv.cHeight));
-        System.out.println("Done");
-
-
-        conv.setvMatrix(5);
-        ImageMatrix image2 = new ImageMatrix(new File("./skyscraper.jpg"));
-        conv.setIMatrix(image2.pixels);
-        conv.Vertical();
-        image.setPixels(conv.newMatrix);
-
-        image.backToImage(Integer.toString(conv.cHeight));
-        System.out.println("Done");
-
-
-        conv.setvMatrix(7);
-        ImageMatrix image3 = new ImageMatrix(new File("./skyscraper.jpg"));
-        conv.setIMatrix(image3.pixels);
-        conv.Vertical();
-        image.setPixels(conv.newMatrix);
-
-        image.backToImage(Integer.toString(conv.cHeight));
-        System.out.println("Done");
-
-        conv.setvMatrix(15);
-        ImageMatrix image4 = new ImageMatrix(new File("./skyscraper.jpg"));
-        conv.setIMatrix(image4.pixels);
-        conv.Vertical();
-        image.setPixels(conv.newMatrix);
-
-        image.backToImage(Integer.toString(conv.cHeight));
-        System.out.println("Done");
 
         //System.out.println(Arrays.deepToString(conv.newMatrix));
 
